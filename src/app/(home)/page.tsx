d9b9fdf07e00390abfd2cf04177772a44fe7eb1b
Punarv Dinakar <tranquilblue91@gmail.com>
2025-06-01 23:59:03 +0530

    minor ui

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,7 +2,7 @@ import { VideoScroll } from "~/components/VideoScroll";
 
 export default async function HomePage() {
   return (
-    <main className="mx-auto min-h-screen max-w-[450px]">
+    <main className="mx-auto min-h-screen max-w-[450px] border-2">
       <VideoScroll />
     </main>
   );
--- src/components/VideoScroll.tsx
+++ src/components/VideoScroll.tsx
@@ -190,7 +190,7 @@ export function VideoScroll() {
   return (
     <div className="relative flex min-h-screen w-full flex-col items-center justify-center">
       <div className="absolute top-0 z-50 w-full max-w-[450px] bg-transparent">
-        <div className="bg-background/80 container mx-auto flex h-16 items-center justify-between border-2 px-4">
+        <div className="bg-background/80 container mx-auto flex h-16 items-center justify-between border-y-2 px-4">
           <div className="flex gap-2">
             <Input
               type="search"
